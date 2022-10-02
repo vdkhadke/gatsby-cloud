03224fc2c875435e581621727fab6995ad01d5f2
Viplav <1767617+vdkhadke@users.noreply.github.com>
2022-10-01 22:57:33 -0400

    Add new branch.

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to page two</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
